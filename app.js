60629d6ca08d1151f6cbb2f969ae5865508471bf
Kelly <kellyaraujoo086@gmail.com>
2025-01-21 23:01:52 -0300

    Exibir lista na tela

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigo = []
+let amigos = []
 
+function exibirLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML="";
+    for (let i = 0; i <amigos.length; i++) {
+        let nome = document.createElement("li");
+        nome.textContent = amigos[i];
+        lista.appendChild(nome);
+        
+    } 
+        
+    
+}
 
-
-function sortearAmigo(){
-    let nomeInput = document.querySelector("nome").value;
+function adicionarAmigo(){
+    let nomeInput = document.getElementById('amigo').value;
+    if (nomeInput) {
+        amigos.push(nomeInput);
+        document.getElementById('amigo').value = '';
+        exibirLista();
+    } else {
+        alert("Por favor, digite um nome")
+    }
 }
 
